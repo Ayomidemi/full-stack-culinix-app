f54050765ef472fbee744d0cedcf09a839415dae
Ayomidemi <adenijiayomide13@gmail.com>
2025-08-22 16:08:15 +0100

    Edited lines

--- client/src/components/DumpComponent.tsx
+++ client/src/components/DumpComponent.tsx
@@ -1,5 +1,13 @@
 const DumpComponent = () => {
-  return <div>DumpComponent</div>;
+  return (
+    <div>
+      <p>Edited line 1</p>
+      <p>Edited line 2</p>
+      <p>Edited line 3</p>
+      <p>Edited line 4</p>
+      <p>Edited line 5</p>
+    </div>
+  );
 };
 
 export default DumpComponent;
