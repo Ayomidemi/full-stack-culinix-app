d891889af5e003c64ac1819964b3e14a6639c43e
Ayomidemi <adenijiayomide13@gmail.com>
2025-08-25 08:33:19 +0100

    Edited lines 6 to 12

--- client/src/components/DumpComponent.tsx
+++ client/src/components/DumpComponent.tsx
@@ -5,6 +5,13 @@ const DumpComponent = () => {
       <p>Edited line 2</p>
       <p>Edited line 3</p>
       <p>Edited line 5</p>
+      <p>Edited line 6</p>
+      <p>Edited line 7</p>
+      <p>Edited line 8</p>
+      <p>Edited line 9</p>
+      <p>Edited line 10</p>
+      <p>Edited line 11</p>
+      <p>Edited line 12</p>
     </div>
   );
 };
