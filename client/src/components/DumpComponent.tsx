76022d0ebdce32880e30f7a0744328bb7f2210cb
Ayomidemi <adenijiayomide13@gmail.com>
2025-08-28 08:39:57 +0100

    Lets take it back to one

--- client/src/components/DumpComponent.tsx
+++ client/src/components/DumpComponent.tsx
@@ -2,52 +2,6 @@ const DumpComponent = () => {
   return (
     <div>
       <p>Edited line 1</p>
-      <p>Edited line 2</p>
-      <p>Edited line 3</p>
-      <p>Edited line 5</p>
-      <p>Edited line 6</p>
-      <p>Edited line 7</p>
-      <p>Edited line 8</p>
-      <p>Edited line 9</p>
-      <p>Edited line 10</p>
-      <p>Edited line 11</p>
-      <p>Edited line 12</p>
-      <p>Edited line 13</p>
-      <p>Edited line 14</p>
-      <p>Edited line 15</p>
-      <p>Edited line 16</p>
-      <p>Edited line 17</p>
-      <p>Edited line 18</p>
-      <p>Edited line 19</p>
-      <p>Edited line 20</p>
-      <p>Edited line 21</p>
-      <p>Edited line 22</p>
-      <p>Edited line 23</p>
-      <p>Edited line 24</p>
-      <p>Edited line 25</p>
-      <p>Edited line 26</p>
-      <p>Edited line 27</p>
-      <p>Edited line 28</p>
-      <p>Edited line 29</p>
-      <p>Edited line 30</p>
-      <p>Edited line 31</p>
-      <p>Edited line 32</p>
-      <p>Edited line 33</p>
-      <p>Edited line 34</p>
-      <p>Edited line 35</p>
-      <p>Edited line 36</p>
-      <p>Edited line 37</p>
-      <p>Edited line 38</p>
-      <p>Edited line 39</p>
-      <p>Edited line 40</p>
-      <p>Edited line 41</p>
-      <p>Edited line 42</p>
-      <p>Edited line 43</p>
-      <p>Edited line 44</p>
-      <p>Edited line 45</p>
-      <p>Edited line 46</p>
-      <p>Edited line 47</p>
-      <p>Edited line 48</p>
     </div>
   );
 };
