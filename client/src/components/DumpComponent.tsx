2116a1480ef9c227f17e85fdd68749d619c9a3fe
Ayomidemi <adenijiayomide13@gmail.com>
2025-08-28 08:40:09 +0100

    Now eight

--- client/src/components/DumpComponent.tsx
+++ client/src/components/DumpComponent.tsx
@@ -2,6 +2,13 @@ const DumpComponent = () => {
   return (
     <div>
       <p>Edited line 1</p>
+      <p>Edited line 2</p>
+      <p>Edited line 3</p>
+      <p>Edited line 4</p>
+      <p>Edited line 5</p>
+      <p>Edited line 6</p>
+      <p>Edited line 7</p>
+      <p>Edited line 8</p>
     </div>
   );
 };
